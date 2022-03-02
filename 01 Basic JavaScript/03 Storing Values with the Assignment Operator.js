bd5c6afd34208697705a69aa33e1c3ae77fe8385
Juan Alberto Glz <juanalbglz@gmail.com>
2022-03-02 00:43:16 +0000

    BJS 7/113

---
+++ 01 Basic JavaScript/03 Storing Values with the Assignment Operator.js
@@ -0,0 +1,36 @@
+/*
+Title:  Storing Values with the Assignment Operator
+URL:    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator
+*/
+
+/* Example
+In JavaScript, you can store a value in a variable with the assignment operator (=).
+
+myVariable = 5;
+This assigns the Number value 5 to myVariable.
+
+IMPORTANT If there are any calculations to the right of the = operator, those are performed before the value is assigned to the variable on the left of the operator.
+
+var myVar;
+myVar = 5;
+First, this code creates a variable named myVar. Then, the code assigns 5 to myVar. Now, if myVar appears again in the code, the program will treat it as if it is 5.
+*/
+
+
+/* Exercise*/
+//TODO Assign the value 7 to variable a.
+
+
+/* Initial content
+// Setup
+var a;
+
+// Only change code below this line
+*/
+
+
+/* Solution */
+// Setup
+var a;
+a = 7;
+// Only change code below this line
